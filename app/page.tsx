b15d8ce751dc723cbe36d77fb8b8de0d2e79cbcb
Bhuv <bhuv059@gmail.com>
2025-02-14 18:50:46 +0100

    tailwind css globals css updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 text-[#FF7000] sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Welcome to Stack overflow
     </div>
   );
