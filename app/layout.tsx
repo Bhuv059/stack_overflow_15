489e4fe56cadc65c19089ff52d8558ed43213d44
Bhuv <bhuv059@gmail.com>
2025-02-16 14:49:35 +0100

    assets forlder, fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import "./globals.css";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
+const inter = localFont({
+  src: "./font/interVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 700 800 900",
+});
+
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "Stack overflow for developers",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -24,9 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 text-[#FF7000] sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Welcome to Stack overflow
+      <h1 className="h1-bold font-inter">Welcome to Stack overflow</h1>
     </div>
   );
 };
